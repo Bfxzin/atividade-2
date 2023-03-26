7ce93f412fb291d6918b12c0e27a7a5712226806
felip <felip@DESKTOP-0VT61A2>
2023-03-26 13:54:37 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite os valores dos lados do triângulo:");
+        int lado1 = int.Parse(Console.ReadLine());
+        int lado2 = int.Parse(Console.ReadLine());
+        int lado3 = int.Parse(Console.ReadLine());
+
+        if (lado1 == lado2 && lado1 == lado3)
+        {
+            Console.WriteLine("O triângulo é equilátero.");
+        }
+        else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+        {
+            Console.WriteLine("O triângulo é isósceles.");
+        }
+        else
+        {
+            Console.WriteLine("O triângulo é escaleno.");
+        }
+    }
+}
